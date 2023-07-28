a7ad480b669df403801f92a52d833ec309610821
Leonardo-Santos <leonardo.co.de23@gmail.com>
2023-07-27 21:34:17 -0300

    Calcular imc finalizado

--- aula/script.js
+++ aula/script.js
@@ -80,32 +80,69 @@ function cadastrarUsuario(dadosUsuario) {
     let listaUsuarios = []
 
     // se ouver uma lista de usuarios no localStorage, carregar isso para a variavel listaUsuarios
-     if (localStorage.getItem("usuariosCadastrados") != null ) {
-        listaUsuarios = JSON.parse( localStorage.getItem("usuariosCadastrados") ) 
+    if (localStorage.getItem("usuariosCadastrados") != null) {
+        listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"))
     }
 
     // adiciona um usuario na lista
     listaUsuarios.push(dadosUsuario)
     // salva a listaUsuarios no localStorage
-    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios) )
+    localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
 
-   
-    
+
+
 }
 
 function carregarUsuarios() {
     let listaCarregada = []
-        if ( localStorage.getItem("usuariosCadastrados") != null) {
-            listaCarregada = JSON.parse(localStorage.getItem("usuariosCadastrados"))
-        }
+    if (localStorage.getItem("usuariosCadastrados") != null) {
+        listaCarregada = JSON.parse(localStorage.getItem("usuariosCadastrados"))
+    }
 
-        if (listaCarregada.length == 0){
-            // Se nao tiver nenhum usuario cadastrado mostra mensagem
-            let tabela = document.getElementById("corpo-tabela")
-            tabela.innerHTML = "Nenhum usuario cadastrado"
-        }
+    if (listaCarregada.length == 0) {
+        // Se nao tiver nenhum usuario cadastrado mostra mensagem
+        let tabela = document.getElementById("corpo-tabela")
+
+        tabela.innerHTML = `<tr class="linha-mensagem">
+            <td colspan="6">Nenhum usuario cadastrado 💀🤡👌</td> </tr>`
+    } else {
+        // montar conteudo da tabela
+        montarTabela(listaCarregada)
+    }
 
     console.log(listaCarregada)
 }
 
-window.addEventListener("DOMContentLoaded", () => carregarUsuarios() )
+window.addEventListener("DOMContentLoaded", () => carregarUsuarios())
+
+
+// Fazer a tabela
+function montarTabela(listaUsuarios) {
+    let tabela = document.getElementById("corpo-tabela")
+
+    let template = ""
+
+    listaUsuarios.forEach(usuario => {
+        console.log("O usuario é: ", usuario)
+
+        template += ` <tr>
+        <td data-cell="nome">${usuario.nome}</td>
+        <td data-cell="altura">${usuario.altura}</td>
+        <td data-cell="peso">${usuario.peso}</td>
+        <td data-cell="valor do IMC">${usuario.imc}</td>
+        <td data-cell="classificação do IMC">${usuario.situacaoImc}</td>
+        <td data-cell="data de cadastro">${usuario.dataCadasdro}</td>
+    </tr> `
+    });
+
+    tabela.innerHTML = template;
+}
+
+function deletarRegistros() {
+
+    // Remove o item do localStorage
+    localStorage.removeItem("usuariosCadastrados")
+    // recarrega a pagina
+    window.location.reload()
+}
+
